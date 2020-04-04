037b390b5ecf0100633a20d86dc16a0e7afaa2ae
fernandosgodoy <santana.fernando@gmail.com>
2020-04-04 06:11:20 -0300

    - chegado na logica correta para o campo decimal

--- framework/formatHelper/decimalNumber.js
+++ framework/formatHelper/decimalNumber.js
@@ -7,9 +7,9 @@ var transformDecimalNumber = function(number, event) {
         case 9:  // Tab
         case 13: // Enter
         case 37: // Left
-        case 38: // Up
+        // case 38: // Up
         case 39: // Right
-        case 40: // Down
+        // case 40: // Down
             return;
     }
 
@@ -18,13 +18,51 @@ var transformDecimalNumber = function(number, event) {
         return;
 
     var formatedMask = "";
-
     if (validateNumberDecimal(number)) {
-        formatedMask += number;
-    } else {
-        var remove = number.toString().substring(0, number.length - 1);
-        return remove;
+        // formatedMask = number + formatedMask;
+        // var numeroCheio = number.toString();
+        var interactWithoutSc = number.replace(",", "");
+        interactWithoutSc = interactWithoutSc.replace(".", "");
+        var interact = interactWithoutSc.split('');
+
+        for(i=0;i<interact.length;i++){
+            console.log(interact[i]);
+// 0,00 (4)
+            // formatedMask += number;
+
+            // 0.000.000 ,00
+            // 0000000000
+
+            if (i==0) //4
+                formatedMask += interactWithoutSc[i];
+            if (i==1) //45
+                formatedMask = "," + formatedMask + interactWithoutSc[i];
+            if (i==2){ //4,50
+                formatedMask = formatedMask.substring(1, 2) +
+                    "," + formatedMask.substring(2, 3) + interactWithoutSc[i];
+            }
+            else if (i==5)
+                formatedMask = "." + formatedMask + interactWithoutSc[i];
+            // else {
+            //     formatedMask += interactWithoutSc[i];
+            // }
+                // formatedMask = numeroCheio.toString().substring(i, );
+            // if (i>4)
+            //     formatedMask = "," + formatedMask;
+        }
     }
+    // formatedMask = parseFloat(number).toFixed(2);
+    // if (validateNumberDecimal(number)) {
+        
+    //     // if (formatedMask.length < 2) {
+    //     //     formatedMask += number;
+    //     // } else if(formatedMask.length == 2) {
+    //     //     formatedMask += number;
+    //     //     formatedMask = "," + formatedMask;
+    //     // } else {
+    //     //     formatedMask = number + formatedMask;
+    //     // }
+    // } 
     
     return formatedMask;
 }
